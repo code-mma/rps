fe44b6068bb6fd01a0c94650d48e9d7be19dc65c
code-mma <michaelangelo.mma@gmail.com>
2022-01-20 22:00:26 +0800

    Create additional comments and modify existing comments

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
 function game() {
     let x = 0;
     while (x <= 4) {
-        const playerSelection =  prompt("What's your choice?").toLowerCase(); //Receive input from player among Rock, Paper or Scissors choices
+        const playerSelection =  prompt("What's your choice?").toLowerCase(); //Receive insensitive character input from player
         const computerSelection = computerPlay(); //Call random computer choices
 
         function computerPlay() {
-            const choices = ['rock', 'paper', 'scissors']; //List choices to be randomized
-            randomNum = Math.floor(Math.random() * choices.length);
-            return choices[randomNum];
+            const choices = ['rock', 'paper', 'scissors']; //Array of choices
+            randomNum = Math.floor(Math.random() * choices.length); //Generate a random number
+            return choices[randomNum]; //Select a random item from the array
         }
 
-        function playRound(playerSelection, computerSelection) {
+        function playRound(playerSelection, computerSelection) { //Check to see if which player is the winner, return the result message
             if (playerSelection == 'rock' && computerSelection == 'paper') {
                 return "You lose! Paper beats rock!";
             }
@@ -30,14 +30,14 @@ function game() {
                 return "You win! Scissors beats paper!";
             }
             else if (playerSelection == computerSelection) {
-                return "Same weapon was chosen. Try again!";
+                return "Same weapon was chosen. Try again!"; //Show when the player and computer's selections are the same
             }
             else {
-                return "Please check if your input is correct.";
+                return "Please check if your input is correct."; //Show when player input has typographical error or incorrect character
             }
         }
         x += 1;
-        console.log("Round " + x + " Result: " + playRound(playerSelection, computerSelection));
+        console.log("Round " + x + " Result: " + playRound(playerSelection, computerSelection)); //Print the result in the console each round
     }
-    console.log("Thank you for playing!");
+    console.log("Thank you for playing!"); //Post message to the player
 }
