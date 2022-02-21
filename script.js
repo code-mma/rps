d2bb9be0ef7d512d79be49ad0f724d10be59f64c
code-mma <michaelangelo.mma@gmail.com>
2022-02-21 20:45:35 +0800

    Add major changes to UI and script

--- script.js
+++ script.js
@@ -1,43 +1,66 @@
-function game() {
-    let x = 0;
-    while (x <= 4) {
-        const playerSelection =  prompt("What's your choice?").toLowerCase(); //Receive a character insensitive input from player
-        const computerSelection = computerPlay();
+function computerChoice() {
+    const choices = ['rock', 'paper', 'scissors'];
+    randomNum = Math.floor(Math.random() * choices.length); //Generate a random number
+    return choices[randomNum];
+}        
 
-        function computerPlay() {
-            const choices = ['rock', 'paper', 'scissors'];
-            randomNum = Math.floor(Math.random() * choices.length); //Generate a random number
-            return choices[randomNum];
-        }
-
-        function playRound(playerSelection, computerSelection) { //Check to see which player is the winner, return the result message
-            if (playerSelection == 'rock' && computerSelection == 'paper') {
-                return "You lose! Paper beats rock!";
-            }
-            else if (playerSelection == 'rock' && computerSelection == 'scissors') {
-                return "You win! Rock beats scissors!";
-            }
-            else if (playerSelection == 'paper' && computerSelection == 'rock') {
-                return "You win! Paper beats rock!";
-            }
-            else if (playerSelection == 'paper' && computerSelection == 'scissors') {
-                return "You lose! Scissors beats paper!";
-            }
-            else if (playerSelection == 'scissors' && computerSelection == 'rock') {
-                return "You lose! Rock beats scissors!";
-            }
-            else if (playerSelection == 'scissors' && computerSelection == 'paper') {
-                return "You win! Scissors beats paper!";
-            }
-            else if (playerSelection == computerSelection) {
-                return "Same weapon was chosen. Try again!"; //Show when the player and computer's selections are the same
-            }
-            else {
-                return "Please check if your input is correct."; //Show when player input has typographical error or incorrect character
-            }
-        }
-        x += 1;
-        console.log("Round " + x + " Result: " + playRound(playerSelection, computerSelection)); //Print the result in the console for each round
+function playRound(playerSelection, computerSelection) { //Check to see which player is the winner, return the result message
+    if (playerSelection == 'rock' && computerSelection == 'paper') {
+        return "You lose! Paper beats rock!";
     }
-    console.log("Thank you for playing!");
+    else if (playerSelection == 'rock' && computerSelection == 'scissors') {
+        return "You win! Rock beats scissors!";
+    }
+    else if (playerSelection == 'paper' && computerSelection == 'rock') {
+        return "You win! Paper beats rock!";
+    }
+    else if (playerSelection == 'paper' && computerSelection == 'scissors') {
+        return "You lose! Scissors beats paper!";
+    }
+    else if (playerSelection == 'scissors' && computerSelection == 'rock') {
+        return "You lose! Rock beats scissors!";
+    }
+    else if (playerSelection == 'scissors' && computerSelection == 'paper') {
+        return "You win! Scissors beats paper!";
+    }
+    else if (playerSelection == computerSelection) {
+        return "Same weapon was chosen. Try again!"; //Show when the player and computer's selections are the same
+    }
+    else {
+        return "Please check if your input is correct."; //Show when player input has typographical error or incorrect character
+    }
+}      
+
+function choosePlay() {
+    const choiceButtons = document.querySelectorAll('.choiceButtons');
+    choiceButtons.forEach((button) => {
+        button.addEventListener('click', () => {
+            console.log(playRound(button.id, computerChoice()));
+        });
+    });  
 }
+
+const startButton = document.querySelector('#start-button');
+
+startButton.addEventListener('click', () => {
+    const startPanel = document.getElementById('start-panel');
+    const choicesArray = ['Rock', 'Paper', 'Scissors'];
+    
+    for (const choice of choicesArray) {
+        const choicesPanel = document.querySelector('#choices-panel');
+        const createButtons = document.createElement('button');
+        createButtons.textContent = choice;
+        createButtons.classList.add('choiceButtons');
+        createButtons.setAttribute('id',choice.toLowerCase());
+        choicesPanel.appendChild(createButtons);
+    }
+
+    startPanel.remove();
+    choosePlay();
+    
+});
+
+
+
+
+  
